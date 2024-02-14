8c050840d167bbeb8a9c6d236c361c2c0cb8fb18
Haley Cahoon <94021394+Colvig@users.noreply.github.com>
2024-02-13 23:05:29 -0500

    updating notes

--- NOTES.blade.php
+++ NOTES.blade.php
@@ -2,14 +2,9 @@
 need to do
 ------------
 - signin footer page needs to link to signin page eventually when we get there
-
 -Header carosel sizing different between pages
--Make spacing consistent everywhere
 -Gray container on meet our team and our mission (might keep change color) Add to all maybe :interrobang:
--Add button to contact page
--Make phone number the same everywhere
-     -Help page and Contact us (123) 456-7890
--Fix all spacing on Track Your Order
+
 -Hyper link mentions of other pages to said other pages
      -FAQS
 -Make carosel work
@@ -24,3 +19,11 @@ done
 - terms of use footer page.....might just be the same thing as terms of service?? 
      remove maybe
 -About us picture missing
+
+-Make spacing consistent everywhere
+-Make phone number the same everywhere
+     -Help page and Contact us (123) 456-7890
+-Fix all spacing on Track Your Order
+-Add button to contact page
+
+
